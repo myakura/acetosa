cca3e45f50a15a82ec6d499e1d4f8a911c3b1512
Masataka Yakura <masataka.yakura@gmail.com>
2021-12-09 21:21:42 +0900

    feat: add handlers for window events

--- acetosa_background.js
+++ acetosa_background.js
@@ -32,4 +32,27 @@ chrome.tabs.onDetached.addListener((tabId, detachInfo) => {
 	countTabs(oldWindowId);
 });
 
+chrome.windows.onCreated.addListener((window) => {
+	console.group(`window created`);
+	console.dir(window);
+	console.log(window.id);
+	console.groupEnd();
+});
+
+chrome.windows.onRemoved.addListener((windowId) => {
+	console.group(`winodow removed`);
+	console.log(windowId);
+	console.groupEnd();
+});
+
+chrome.windows.onFocusChanged.addListener((windowId) => {
+	console.group(`window focus changed`);
+	console.log(windowId);
+	console.groupEnd();
+	windowId > 0 && chrome.windows.get(windowId, { populate: true }, (window) => {
+		console.log(`window for `, windowId);
+		console.dir(window);
+	});
+});
+
 countTabs();
